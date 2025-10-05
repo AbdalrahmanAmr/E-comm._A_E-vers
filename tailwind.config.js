9330e8a43abbd00366ecdc9cca63f11253c0480f
Mr.33 <133975665+AbdalrahmanAmr@users.noreply.github.com>
2025-10-05 15:16:14 +0300

    Clean Bolt files, update Vite & Tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  darkMode: "class", // Enables dark/light mode
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00ADB5",
+        secondary: "#393E46",
+      },
+    },
   },
   plugins: [],
 };
